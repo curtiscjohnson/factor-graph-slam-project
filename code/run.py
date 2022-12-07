861634cb5fc65f6ad7c2fff527d9c342d13801a8
shawk4 <shawk13013@gmail.com>
2022-12-07 16:07:17 -0700

    halfway decent with average noise

--- code/run.py
+++ code/run.py
@@ -10,10 +10,10 @@ def run():
 
     numSteps = 200
     # System Noise parameters
-    # alphas = np.array([0.05, 0.001, 0.05, 0.01])**2 # These have to do with the error in our motion controls
-    # betas = np.array([10, 10*np.pi/180]) # Error in observations
-    alphas = np.array([0.001, 0.001, 0.001, 0.001])**2 # These have to do with the error in our motion controls
-    betas = np.array([0.001, 0.001]) # Error in observations
+    alphas = np.array([0.05, 0.001, 0.05, 0.01])**2 # These have to do with the error in our motion controls
+    betas = np.array([10, 10*np.pi/180]) # Error in observations
+    # alphas = np.array([0.001, 0.001, 0.001, 0.001])**2 # These have to do with the error in our motion controls
+    # betas = np.array([0.001, 0.001]) # Error in observations
     # alphas = np.array([0.00, 0.00, 0.00, 0.00])**2 # These have to do with the error in our motion controls
     # betas = np.array([0.00, 0.00]) # Error in observations
     soccer_bot = simulation(numSteps=numSteps,alphas=alphas,betas=betas)
@@ -22,11 +22,11 @@ def run():
     cov= 1e-03*np.eye(3) # start covariance low but non-zero
     ekf_SLAM_alg = ekf_slam(mu,cov,alphas=alphas,betas=betas)
 
-    graph_alg = graph_slam_known(mu, prior_sigmas=np.array([0,0,0]),
-                                            minK=20,
-                                            incK=5,
-                                            alphas=alphas,
-                                            betas=betas)
+    graph_alg = graph_slam_known(mu,
+                                    minK=20,
+                                    incK=5,
+                                    alphas=alphas,
+                                    betas=betas)
 
     #=================================================
     # Parameters: 
